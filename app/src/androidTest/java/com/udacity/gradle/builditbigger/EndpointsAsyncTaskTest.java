f1ce7c19bd405ba6f8c48ed2d31b4fa5ee5dcb54
Kareem Farag <kareemfarag@hotmail.com>
2021-03-15 12:40:57 +0200

    Refactoring Test Add Test to assert if return value from asynctask is not empty

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -4,6 +4,8 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.concurrent.ExecutionException;
@@ -12,7 +14,7 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest  {
-
+    @Test
     public void testAsyncTask () throws ExecutionException, InterruptedException {
         EndpointsAsyncTask endpointsAsyncTask =new EndpointsAsyncTask(new EndpointsAsyncTask.Callback(){
             @Override
