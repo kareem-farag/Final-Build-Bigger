6cd517070df48448b10203d43e81d86f05c5c263
Kareem Farag <kareemfarag@hotmail.com>
2021-03-15 12:36:41 +0200

    Feat Add Test to assert if return value from asynctask is not empty

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,26 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
+
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest  {
+
+    public void testAsyncTask () throws ExecutionException, InterruptedException {
+        EndpointsAsyncTask endpointsAsyncTask =new EndpointsAsyncTask(new EndpointsAsyncTask.Callback(){
+            @Override
+            public void onCallFinished(String result) {
+
+            }
+        });
+        endpointsAsyncTask.execute();
+        assertNotNull(endpointsAsyncTask.get());
+    }
+}
